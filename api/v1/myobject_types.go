b7eff8326950d2c41744e4c601251b33485a2c60
阿少 <xushaoyu.xsy@alibaba-inc.com>
2023-06-29 14:18:15 +0800

    feat: init CRD

---
+++ api/v1/myobject_types.go
@@ -0,0 +1,67 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// MyObjectSpec defines the desired state of MyObject
+type MyObjectSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of MyObject. Edit myobject_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+
+	//
+	Attribute string `json:"attribute,omitempty"`
+}
+
+// MyObjectStatus defines the observed state of MyObject
+type MyObjectStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// MyObject is the Schema for the myobjects API
+type MyObject struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   MyObjectSpec   `json:"spec,omitempty"`
+	Status MyObjectStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// MyObjectList contains a list of MyObject
+type MyObjectList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []MyObject `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&MyObject{}, &MyObjectList{})
+}
